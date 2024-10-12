b97aea6b21cb21b836f9282385fe5f813a21fd16
JoshVarughese <144305737+JoshVarughese@users.noreply.github.com>
2024-10-12 12:31:09 -0500

    Initial Initialzr Project

---
+++ jjfc-super-turbo-service/jjfc-super-turbo-service/src/main/java/com/jjfc/jjfc_super_turbo_service/JjfcSuperTurboServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jjfc.jjfc_super_turbo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjfcSuperTurboServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjfcSuperTurboServiceApplication.class, args);
+	}
+
+}
---
+++ jjfc-super-turbo-service/jjfc-super-turbo-service/src/test/java/com/jjfc/jjfc_super_turbo_service/JjfcSuperTurboServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjfc.jjfc_super_turbo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjfcSuperTurboServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
